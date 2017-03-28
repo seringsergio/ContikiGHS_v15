6f2b44b7435f29f846f31cf785b6f8f58b354205
root <diaz-sergio@javeriana.edu.co>
2017-03-28 11:38:19 -0500

    2017-03-28

--- examples/ghs_algoritm/results/topology/matrixConnection/parserLogListener.cpp
+++ examples/ghs_algoritm/results/topology/matrixConnection/parserLogListener.cpp
@@ -9,26 +9,28 @@ int main()
 
     std::ifstream file("loglistener.txt");
     std::string line;
-    std::string readStr = "READ";
+    std::string readStr = "END";
 
     while (std::getline(file, line)) // Saco linea por linea del archivo
     {
-        if (line.find(readStr) != std::string::npos) //Si la linea contiene la string "READ"
+        if (line.find(readStr) != std::string::npos) //Si la linea contiene la string "END"
         {
             std::istringstream ss(line);
 
             //Variable para guardar los datos que vienen en el archivo
             std::string Time;
             std::string Mote;
-            std::string READ;
+            std::string END;
             std::string id;
-            std::string neighbor;
-            double avg_seqno_gap;
+            std::string parent;
+            double weight;
+            std::string Branch;
+            double Name;
 
             //Sacar los todos los valores de la linea que estan separados por espacio
-            ss >> Time >> Mote >> READ >> id >> neighbor >> avg_seqno_gap;
+            ss >> Time >> Mote >> END >> id >> parent >> weight >> Branch >> Name;
             //Imprimir los valores guardados
-            std::cout << id << " " << neighbor << " " << avg_seqno_gap  << " 0" << " 0" << "\n";
+            std::cout << id << " " << parent << " " << weight  << " 0" << " 0 " <<  Branch << " " <<  Name <<"\n";
 
         }
     }
